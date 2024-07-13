99f49abf7ea68181048f74e923ecdbb28d7c2c5c
Rashmi-Salunkhe <158038788+Rashmi-Salunkhe@users.noreply.github.com>
2024-07-13 11:20:46 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,6 +1,6 @@
 function fibonacci(num) {
 // your code here
-	if (num === 0) return 0;
+	 if (num === 0) return 0;
   if (num === 1) return 1;
 
   let a = 0, b = 1;
@@ -9,7 +9,7 @@ function fibonacci(num) {
     a = b;
     b = next;
   }
-  return b;
+  return a;
 }
 
 module.exports = fibonacci;
