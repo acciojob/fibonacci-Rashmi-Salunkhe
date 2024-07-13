324afecbab598ecca5401f281e636c64bb7f3157
Rashmi-Salunkhe <158038788+Rashmi-Salunkhe@users.noreply.github.com>
2024-07-13 11:26:03 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,15 +1,16 @@
 function fibonacci(num) {
 // your code here
-	 if (num === 0) return 0;
-  if (num === 1) return 1;
+	 if (num === 0) return 0; // First Fibonacci number
+  if (num === 1) return 0; // Second Fibonacci number
+  if (num === 2) return 1; // Third Fibonacci number
 
   let a = 0, b = 1;
-  for (let i = 2; i <= num; i++) {
+  for (let i = 3; i <= num; i++) {
     let next = a + b;
     a = b;
     b = next;
   }
-  return a;
+  return b;
 }
 
 module.exports = fibonacci;
